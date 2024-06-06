9cdf193381b3605d899572ce110422b985a5c00b
Raydner <151770446+GuilhermeFlex@users.noreply.github.com>
2024-06-06 13:24:57 -0300

    Feito ate a aula  11

--- curso-react/src/components/Form.jsx
+++ curso-react/src/components/Form.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
+
 function Form() {
   function cadastrarUsuario(e) {
-    e.preventDefault()
-    console.log("User cadastrado");
+    e.preventDefault();
+    console.log(`Usuario ${nome} foi cadastrado com a senha ${senha}`);
   }
 
+  const[nome, setNome] = useState('Raydner')
+  const[senha, setSenha] = useState(' ')
+  
   return (
     <>
       <h1>Meu cadastro</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="digite o seu nome" />
+          <label htmlFor="nome">Nome:</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            placeholder="digite o seu nome"
+            onChange={(e) => setNome(e.target.value)}
+            value={nome}
+          />
+        </div>
+        <div>
+          <label htmlFor="senha">Senha:</label>
+          <input
+            type="text"
+            id="senha"
+            name="senha"
+            placeholder="Digite a sua senha"
+            onChange={(e) => setSenha(e.target.value)}
+            value={senha}
+          />
+        </div>
+        <div>
           <input type="submit" value="Cadastrar" />
         </div>
       </form>
